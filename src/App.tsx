34de49aa936f7aea733503b6b774971053d0387a
DanilAgent <bion-1@mail.ru>
2024-10-15 09:46:37 +0200

    com

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,15 @@ import Main from "./components/Main.tsx";
 import {v1} from "uuid";
 import {useState} from "react";
 import AddTodo from "./components/AddTodo.tsx";
-import DeleteTodo from "./components/DeleteTodo.tsx";
 
-type TypeArr = {
+export type TypeArr = {
     id: string,
     todo: string,
     check: boolean
 }
 
 function App() {
-    const [arr, setArr] = useState <TypeArr>({
+    const [arr] = useState <TypeArr>({
         id: v1(),
         todo: "My first todo",
         check: false
@@ -21,8 +20,7 @@ function App() {
   return (
     <>
         <AddTodo/>
-        <Main arr={arr} setArr={setArr}/>
-        <DeleteTodo/>
+        <Main arr={arr}/>
     </>
   )
 }
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,26 @@
+import {TypeArr} from "../App.tsx";
+import {ChangeEvent, FC} from "react";
 
-const Main = ({arr, setArr}) => {
+type PropsMain = {
+    arr: TypeArr,
+    // setArr: () => void,
+}
+
+const Main: FC<PropsMain> = ({arr}) => {
+    const onChangeCheck = (e: ChangeEvent<HTMLInputElement>) =>
+        console.log(e.currentTarget.value)
+
+
     return (
         <div>
             <div className="mx-16 min-h-8 w-auto bg-gray-400 p-2 flex">
                 <div>{arr.todo}</div>
                 <div>
-                    <input type="checkbox" checked={arr.check} onClick={setArr(!arr.check)}/>
+                    <input
+                        type="checkbox"
+                        checked={arr.check}
+                        onChange={onChangeCheck}
+                    />
                 </div>
             </div>
         </div>
